f00ea76ac218ef707d2b00499a1482d05e860c65
José Carlos <jcolscript@gmail.com>
2019-12-10 02:16:46 -0300

    configuracao dos validators em cada rota

--- start/routes.js
+++ start/routes.js
@@ -18,6 +18,6 @@ Route.get('/', () => {
   return { greeting: 'API enabled' };
 });
 
-Route.post('/sessions', 'SessionController.store');
-Route.post('/forgot', 'ForgotPasswordController.store');
-Route.post('/reset', 'ResetPasswordController.store');
+Route.post('/sessions', 'SessionController.store').validator('Session');
+Route.post('/forgot', 'ForgotPasswordController.store').validator('Forgot');
+Route.post('/reset', 'ResetPasswordController.store').validator('Reset');
